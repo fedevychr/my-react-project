6c9069191e8c9673ba8207545c91b92296069934
Roman Fedevych <fedevychr@gmail.com>
2024-03-14 00:10:04 +0200

    fix styles mod 1.2

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -4,16 +4,16 @@ import clsx from "clsx";
 import css from "./Alert.module.css";
 
 const Alert = ({ variant, outlined, elevated, children }) => {
-  return (
-    <p
-      className={clsx(css[variant], {
-        [css.isOutlined]: outlined,
-        [css.isElevated]: elevated,
-      })}
-    >
-      {children}
-    </p>
-  );
+	return (
+		<p
+			className={clsx(css[variant], {
+				[css.isOutlined]: outlined,
+				[css.isElevated]: elevated,
+			})}
+		>
+			{children}
+		</p>
+	);
 };
 
 
