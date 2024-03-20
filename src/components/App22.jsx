ebe2f0b55df5451897c7b12318cd02c4759f4617
Roman Fedevych <fedevychr@gmail.com>
2024-03-20 23:16:10 +0200

    modal, useEffect, LocalStorage to mod22

--- src/components/App22.jsx
+++ src/components/App22.jsx
@@ -1,1 +1,67 @@
+// import { useState } from 'react';
+// import Modal from './Modal/Modal';
 
+// const App22 = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   return (
+//     <div>
+//       <button onClick={() => setIsOpen(!isOpen)}>
+//         {isOpen ? 'Close' : 'Open'}
+//       </button>
+//       {isOpen && <Modal />}
+//     </div>
+//   );
+// };
+
+// import { useState, useEffect } from 'react';
+
+// const App22 = () => {
+//   const [first, setFirst] = useState(0);
+//   const [second, setSecond] = useState(0);
+
+//   useEffect(() => {
+//     console.log('First updated: ', first);
+//   }, [first]);
+
+//   useEffect(() => {
+//     console.log('Second updated: ', second);
+//   }, [second]);
+
+//   useEffect(() => {
+//     console.log('First or second updated: ', first + second);
+//   }, [first, second]);
+
+//   return (
+//     <>
+//       <button onClick={() => setFirst(first + 1)}>First: {first}</button>
+//       <button onClick={() => setSecond(second + 1)}>Second: {second}</button>
+//     </>
+//   );
+// };
+
+import { useEffect, useState } from 'react';
+
+const App22 = () => {
+  const [clicks, setClicks] = useState(() => {
+    const savedClicks = window.localStorage.getItem('saved-clicks');
+    if (savedClicks !== null) {
+      return savedClicks;
+    }
+    return 0;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem('saved-clicks', clicks);
+  }, [clicks]);
+
+  return (
+    <div>
+      <button onClick={() => setClicks(clicks + 1)}>
+        You clicked {clicks} times
+      </button>
+      <button onClick={() => setClicks(0)}>Reset</button>
+    </div>
+  );
+};
+
+export default App22;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+
+const Modal = () => {
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      console.log(`Interval -${Date.now()}`);
+    }, 2000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return <div>Modal</div>;
+};
+
+export default Modal;
