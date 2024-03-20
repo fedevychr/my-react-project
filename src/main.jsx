1bcf9ae24ab1a7f7ab87b6b1d98180318fa4894b
Roman Fedevych <fedevychr@gmail.com>
2024-03-20 21:19:08 +0200

    modern index, main, creat app22

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './components/App';
+// import App1 from './components/App1';
+// import App12 from './components/App12';
+// import App21 from './components/App21';
+import App22 from './components/App22';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   <React.StrictMode>
-      <App />
-   </React.StrictMode>,
-)
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <App1 /> */}
+    {/* <App12 /> */}
+    {/* <App21 /> */}
+    <App22 />
+  </React.StrictMode>,
+);
